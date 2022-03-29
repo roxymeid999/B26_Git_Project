5b2ea0c8808706610d9936f0a7d74f78926bf398
roxymeid999 <rok.zak@iclou.com>
2022-03-28 20:58:43 -0700

    Changed first

--- src/day0/First.java
+++ src/day0/First.java
@@ -3,5 +3,9 @@ package day0;
 public class First {
     public static void main(String[] args) {
         // TODO: Work on it tomorrow
+
+        // extra line 1
+        // extra line 2
+        // extra line 3
     }
 }
